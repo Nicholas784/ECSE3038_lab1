b5c4b87397632cfc6b85b879428d7755f2fed38d
Nicholas784 <nicholasdeane.nd@gmail.com>
2021-01-29 14:05:24 -0500

    Second Function Added

--- ECSE3038_Lab1.py
+++ ECSE3038_Lab1.py
@@ -2,3 +2,26 @@ def hello():
     print("ECSE3038 - Engineering IoT Systems")
 
 hello()
+
+def validatePassword(password):
+
+    numCount = 0;
+
+    if len(password) > 7 and password.isalnum():
+        
+        for character in password:
+
+            if character.isnumeric():
+                numCount += 1
+        
+        return True if numCount >= 2 else False
+
+    else:
+        return False
+
+
+password = input("Please enter your password: ")
+
+print(validatePassword(password))
+
+
